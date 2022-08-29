bc2cd3ff93fe36b4a8d71ae36ca6c39609e81353
LDG - DGit <hi-ldg@dgit.co>
2022-08-30 01:40:22 +0900

    Create 4-1 lists.py

---
+++ Basic Theory/4-1 lists.py
@@ -0,0 +1,17 @@
+days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+print(days_of_week[2])
+
+days_of_week.clear()
+
+print(days_of_week)
+
+days_of_week.append("Week")
+
+print(days_of_week)
+
+list = [1, 2, "a", "b", True, False, [1, 2, 3]]
+
+print(list)
+
+# 리스트에는 어떤 자료형을 넣어도 된다.
