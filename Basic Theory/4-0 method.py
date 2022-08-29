62ca52f8199107690791f4dafd538285624e80c5
LDG - DGit <hi-ldg@dgit.co>
2022-08-29 17:15:11 +0900

    Create 4-0 method.py

---
+++ Basic Theory/4-0 method.py
@@ -0,0 +1,10 @@
+days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+name = "bobby"
+
+print(name.capitalize())
+print(name.startswith("b"))
+print("bobby".startswith("a"))
+
+#list, tuple, dictionary
+# 저렇게 string 변수에는 다양한 함수가 들어있다
