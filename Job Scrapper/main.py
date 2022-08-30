e45ecc2776fb94bd6777aced17bd84cf1157e1f1
Bobby Lee <hi-ldg@dgit.co>
2022-08-30 07:10:02 +0000

    load job posts

--- Job Scrapper/main.py
+++ Job Scrapper/main.py
@@ -11,3 +11,9 @@ if response.status_code != 200:
 else:
     soup = BeautifulSoup(response.text, "html.parser")
     jobs = soup.find_all("section", class_="jobs")
+    for job_section in jobs:
+        job_posts = job_section.find_all('li')
+        job_posts.pop(-1)
+        for post in job_posts:
+            print(post)
+            print("--------")
