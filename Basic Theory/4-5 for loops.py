a5c846b8685cb6a696f543f04e55590df92a151d
LDG - DGit <hi-ldg@dgit.co>
2022-08-30 10:20:31 +0900

    Create 4-5 for loops.py

---
+++ Basic Theory/4-5 for loops.py
@@ -0,0 +1,15 @@
+import webbrowser
+
+
+websites = [
+    "google.com",
+    "airbnb.com",
+    "twitter.com",
+    "facebook.com",
+    "tiktok.com"
+]
+
+for website in websites:
+    print(website)
+
+# 관습적인건데 사람들이 list나 tuple 을 만들때에는 복수형을 사용한다. websites, movies, users 처럼. 그리고 for 같은거 다음에는 단수로 쓴다. website, movie 같이
