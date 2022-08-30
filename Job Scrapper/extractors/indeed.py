c4fdf8c190bad30032a1912d28da0992b4026419
LDG - DGit <hi-ldg@dgit.co>
2022-08-30 23:08:34 +0900

    classify job data

--- Job Scrapper/extractors/indeed.py
+++ Job Scrapper/extractors/indeed.py
@@ -21,5 +21,8 @@ soup = BeautifulSoup(browser.page_source, "html.parser")
 job_list = soup.find('ul', class_="jobsearch-ResultsList")
 jobs = job_list.find_all('li', recursive=False)
 for job in jobs:
-    print(job)
-    print("------")
+    zone = job.find("div", class_="mosaic-zone")
+    if zone == None:
+        print("job li")
+    else:
+        print("mosaic")
