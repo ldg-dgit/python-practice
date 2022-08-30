9cd44630a7661cdf17ffc9e6c9da02f54805e4ab
LDG - DGit <hi-ldg@dgit.co>
2022-08-30 17:37:01 +0900

    extract result

--- Job Scrapper/main.py
+++ Job Scrapper/main.py
@@ -9,11 +9,23 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
 else:
+    result = []
     soup = BeautifulSoup(response.text, "html.parser")
     jobs = soup.find_all("section", class_="jobs")
     for job_section in jobs:
         job_posts = job_section.find_all('li')
         job_posts.pop(-1)
         for post in job_posts:
-            print(post)
-            print("--------")
+            anchors = post.find_all('a')
+            anchor = anchors[1]
+            link = anchor['href']
+            company, kind, region = anchor.find_all('span', class_='company')
+            anchor.find_all('span', class_="title")
+            title = anchor.find('span', class_='title')
+            job_data = {
+                'company': company.string,
+                'employed_form': kind.string,
+                'region': region.string,
+                'position': title.string
+            }
+            result.append(job_data)
