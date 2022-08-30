31fb2ad9b5ca2a8f51ffbd51017f244335e0f20e
LDG - DGit <hi-ldg@dgit.co>
2022-08-30 15:29:12 +0900

    Init scrapping with BeautifulSoup

---
+++ Job Scrapper/main.py
@@ -0,0 +1,13 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
+
+response = get(f"{base_url}{search_term}")
+
+if response.status_code != 200:
+    print("Can't request website")
+else:
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all("section", class_="jobs")
