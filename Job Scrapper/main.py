7b1084a57b5d338b0883829a85967ff440db6685
LDG - DGit <hi-ldg@dgit.co>
2022-09-06 08:47:22 +0900

    add flask

--- Job Scrapper/main.py
+++ Job Scrapper/main.py
@@ -1,23 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, send_file
+from extractors.indeed import extract_jobs_indeed
+from extractors.we_work_remotely import extract_jobs_wwr
+from file import save_to_file
 
-app = Flask("Job_Scrapper")
+app = Flask("JobScrapper")
+
+db = {}
 
 
 @app.route("/")
-def home():
-    return 'hey there!'
+def index():
+    return render_template("home.html")
 
 
-app.run()
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        indeed = extract_jobs_indeed(keyword)
+        wwr = extract_jobs_wwr(keyword)
+        jobs = indeed + wwr
+        db[keyword] = jobs
+    return render_template("search.html", keyword=keyword, jobs=jobs)
 
 
-# from extractors.we_work_remotely import extract_jobs_wwr
-# from extractors.indeed import extract_jobs_indeed
-# from file import save_to_file
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}_job.csv", as_attachment=True)
 
-# keyword = input("What do you want to search for?")
-# wwr = extract_jobs_wwr(keyword)
-# indeed = extract_jobs_indeed(keyword)
-# jobs = indeed + wwr
 
-# save_to_file(keyword, jobs)
+app.run("localhost")
